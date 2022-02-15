132fbd6ed2771b32896dc3eea185095528780df7
qqqys <2636775389@qq.com>
2022-02-15 23:44:33 +0800

    提交

---
+++ src/components/WebSocket.vue
@@ -0,0 +1,102 @@
+<template>
+</template>
+
+<script>
+    export default {
+        name: "WebSocket",
+        data() {
+            return {
+                // ws是否启动
+                wsIsRun: false,
+                // 定义ws对象
+                webSocket: null,
+                // ws请求链接（类似于ws后台地址）
+                ws: '',
+                // ws定时器
+                wsTimer: null,
+            }
+        },
+        async mounted() {
+            this.wsIsRun = true
+            this.wsInit()
+        },
+        methods: {
+            sendDataToServer() {
+                if (this.webSocket.readyState === 1) {
+                    this.webSocket.send('来自前端的数据')
+                } else {
+                    throw Error('服务未连接')
+                }
+            },
+            /**
+             * 初始化ws
+             */
+            wsInit() {
+                const wsuri = 'ws://localhost:8888/websocket/admin'
+                this.ws = wsuri
+                if (!this.wsIsRun) return
+                // 销毁ws
+                this.wsDestroy()
+                // 初始化ws
+                this.webSocket = new WebSocket(this.ws)
+                // ws连接建立时触发
+                this.webSocket.addEventListener('open', this.wsOpenHanler)
+                // ws服务端给客户端推送消息
+                this.webSocket.addEventListener('message', this.wsMessageHanler)
+                // ws通信发生错误时触发
+                this.webSocket.addEventListener('error', this.wsErrorHanler)
+                // ws关闭时触发
+                this.webSocket.addEventListener('close', this.wsCloseHanler)
+
+                // 检查ws连接状态,readyState值为0表示尚未连接，1表示建立连接，2正在关闭连接，3已经关闭或无法打开
+                clearInterval(this.wsTimer)
+                this.wsTimer = setInterval(() => {
+                    if (this.webSocket.readyState === 1) {
+                        clearInterval(this.wsTimer)
+                    } else {
+                        console.log('ws建立连接失败')
+                        this.wsInit()
+                    }
+                }, 3000)
+            },
+            wsOpenHanler(event) {
+                console.log('ws建立连接成功')
+            },
+            wsMessageHanler(e) {
+                this.$emit('cmsg', e.data)
+            },
+            /**
+             * ws通信发生错误
+             */
+            wsErrorHanler(event) {
+                console.log(event, '通信发生错误')
+                this.wsInit()
+            },
+            /**
+             * ws关闭
+             */
+            wsCloseHanler(event) {
+                console.log(event, 'ws关闭')
+                this.wsInit()
+            },
+            /**
+             * 销毁ws
+             */
+            wsDestroy() {
+                if (this.webSocket !== null) {
+                    this.webSocket.removeEventListener('open', this.wsOpenHanler)
+                    this.webSocket.removeEventListener('message', this.wsMessageHanler)
+                    this.webSocket.removeEventListener('error', this.wsErrorHanler)
+                    this.webSocket.removeEventListener('close', this.wsCloseHanler)
+                    this.webSocket.close()
+                    this.webSocket = null
+                    clearInterval(this.wsTimer)
+                }
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
-
-console.log("hello")
+createApp(App)
+    .use(ElementPlus)
+    .mount('#app')
