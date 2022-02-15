09436d84392277b978c03c809b08469ed3227b1f
Stan <1972446021@qq.com>
2022-02-15 23:13:01 +0800

    函数

--- src/function/addTrack.js
+++ src/function/addTrack.js
@@ -1,9 +1,10 @@
 import * as Cesium from "cesium";
 const addTrack = function(viewer, Track) {
     const line = [];
-    for (var i = 0; i < Track.TrackPoints.length; i += 3) {
+    for (var i = 0; i < Track.TrackPoints.length; i += 30) {
         line.push(Cesium.Cartesian3.fromArray(Track.TrackPoints, i));
     }
+    line.push(line[0])
 
     viewer.entities.add({
         name: Track.TargetObject,
