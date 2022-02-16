7eef79f94e5ce79136bdd74ffec70baefa921a36
Stan <1972446021@qq.com>
2022-02-16 08:16:09 +0800

    修改addTrack函数，不仅能添加轨道，也能改变轨道

--- src/function/addTrack.js
+++ src/function/addTrack.js
@@ -1,20 +1,28 @@
 import * as Cesium from "cesium";
 const addTrack = function(viewer, Track) {
     const line = [];
-    for (var i = 0; i < Track.TrackPoints.length; i += 30) {
+    const entity = viewer.entities.getById(Track.TargetObject)
+    const interval = Track.TrackPoints.length / 10
+
+    for (var i = 0; i < Track.TrackPoints.length; i += interval) {
         line.push(Cesium.Cartesian3.fromArray(Track.TrackPoints, i));
     }
     line.push(line[0])
 
-    viewer.entities.add({
-        name: Track.TargetObject,
-        id: Track.TargetObject,
-        polyline: {
-            positions: line,
-            width: Track.LineWidth,
-            material: Cesium.Color.fromCssColorString("rgb" + Track.Color),
-        },
-    });
+    if (!entity) {
+        viewer.entities.add({
+            name: Track.TargetObject,
+            id: Track.TargetObject,
+            polyline: {
+                positions: line,
+                width: Track.LineWidth,
+                material: Cesium.Color.fromCssColorString("rgb" + Track.Color),
+            },
+        });
+    } else {
+        entity.polyline.positions = line
+    }
+
 };
 
 export default addTrack
