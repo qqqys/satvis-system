c99d364bf95714f827cd9803b8f22da78047d8d6
Stan <1972446021@qq.com>
2022-02-17 10:29:08 +0800

    添加了一个新的连线函数，对之前函数进行了修改

---
+++ src/function/addLine.js
@@ -0,0 +1,25 @@
+import * as Cesium from "cesium";
+const addLine = function(viewer, line) {
+    const pos1 = viewer.entities.getById(line.LineStartObject).position._value;
+    const pos2 = viewer.entities.getById(line.LineEndObject).position._value;
+    let material
+    if (line.LineType === 1.0)
+        material = Cesium.Color.fromCssColorString("rgb" + line.Color)
+    else {
+        material = new Cesium.PolylineDashMaterialProperty({
+            color: Cesium.Color.fromCssColorString("rgb" + line.Color),
+            dashLength: 20, //短划线长度
+        })
+    }
+    viewer.entities.add({
+        id: line.TargetObject,
+        polyline: {
+            arcType: Cesium.ArcType.NONE,
+            positions: [pos1, pos2],
+            width: line.LineWidth,
+            material: material,
+        },
+    });
+};
+
+export default addLine
--- src/function/addTrack.js
+++ src/function/addTrack.js
@@ -1,26 +1,25 @@
 import * as Cesium from "cesium";
 const addTrack = function(viewer, Track) {
-    const line = [];
+    const position = [];
     const entity = viewer.entities.getById(Track.TargetObject)
-    const interval = Track.TrackPoints.length / 10
 
-    for (var i = 0; i < Track.TrackPoints.length; i += interval) {
-        line.push(Cesium.Cartesian3.fromArray(Track.TrackPoints, i));
+    for (var i = 0; i < Track.TrackPoints.length; i += 3) {
+        position.push(Cesium.Cartesian3.fromArray(Track.TrackPoints, i));
     }
-    line.push(line[0])
+    // line.push(line[0])
 
     if (!entity) {
         viewer.entities.add({
             name: Track.TargetObject,
             id: Track.TargetObject,
             polyline: {
-                positions: line,
+                positions: position,
                 width: Track.LineWidth,
                 material: Cesium.Color.fromCssColorString("rgb" + Track.Color),
             },
         });
     } else {
-        entity.polyline.positions = line
+        entity.polyline.positions = position
     }
 
 };
