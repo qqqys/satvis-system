42f7316bd54c3d0d00ac686efadd4bbf82c583b8
qqqys <2636775389@qq.com>
2022-03-25 19:52:36 +0800

    commit

--- src/function/createBeam.js
+++ src/function/createBeam.js
@@ -1,16 +1,26 @@
 import * as Cesium from "cesium";
+import { ElMessage } from "element-plus";
 const createBeam = function (viewer, Beam) {
   let satellite = viewer.entities.getById(Beam.BeamOwner);
+  if(satellite === undefined){
+    ElMessage.warning("波束"+Beam.TargetObject+"对应的卫星"+Beam.BeamOwner+"不存在, 波束添加失败")
+    return
+  }
   viewer.entities.add({
     id: Beam.TargetObject,
-    position:satellite.position._value,
+    position:satellite.position?._value,
+    type: Beam.BeamType,
     cylinder: {
       length:Beam.BeamLength,
       topRadius: 0.0,
       bottomRadius: Math.tan(Beam.BeamAngle*180/3.14)*Beam.BeamLength,
-      material: Cesium.Color.RED.withAlpha(1),
+      material: Cesium.Color.fromCssColorString("rgb" + Beam.Color).withAlpha(Beam.Transparency),
+      outline: true,
+      outlineColor: Cesium.Color.fromCssColorString("rgb" + Beam.Color).withAlpha(Beam.Transparency),
+      outlineWidth: 1.0
     },
   })
+  console.log(viewer.entities.getById(Beam.TargetObject));
 };
 
 export default createBeam
